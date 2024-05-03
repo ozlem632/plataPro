de01fcec4bb1f130910a7ee1b78a3959bc82a068
ozlem <ozlemasimoglu@gmail.com>
2024-05-03 19:39:24 +0200

    ornekjsonanillo sinifinda degisikler

---
+++ productos.js
@@ -0,0 +1,159 @@
+let cartItems = [
+    {
+        "urun_id": "yuzuk_001",
+        "marka": "PLATAPRO",
+        "model": "",
+        "renk": "Altin",
+        "tasarim": "Yildiz",
+        "fiyat": 1500,
+        "detay": {
+            "malzeme": "Altin",
+            "tasarim_detayi": "Yildiz şeklinde tasarım",
+            "tasima_ozelligi": "Kutusunda güvenli bir şekilde taşınabilir"
+        },
+        "img_url": "IMG/yuzuka.jpeg",
+    },
+    {
+        "urun_id": "yuzuk_002",
+        "marka": "PLATAPRO",
+        "model": "Model2",
+        "renk": "Gumus",
+        "tasarim": "Kalp",
+        "fiyat": 1200,
+        "detay": {
+            "malzeme": "Gümüş",
+            "tasarim_detayi": "Kalp şeklinde tasarım",
+            "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+        },
+        "img_url": "IMG/yuzukb.jpeg"
+    },{
+        "urun_id": "yuzuk_002",
+        "marka": "PLATAPRO",
+        "model": "Model2",
+        "renk": "Gumus",
+        "tasarim": "Kalp",
+        "fiyat": 1200,
+        "detay": {
+            "malzeme": "Gümüş",
+            "tasarim_detayi": "Kalp şeklinde tasarım",
+            "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+        },
+        "img_url": "IMG/yuzuk7.jpeg"
+    },{
+        "urun_id": "yuzuk_002",
+        "marka": "PLATAPRO",
+        "model": "Model2",
+        "renk": "Gumus",
+         "tasarim": "Kalp",
+        "fiyat": 1200,
+        "detay": {
+            "malzeme": "Gümüş",
+            "tasarim_detayi": "Kalp şeklinde tasarım",
+            "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+        },
+        "img_url": "IMG/YUZUK3.jpeg"
+    }, {
+        "urun_id": "yuzuk_002",
+        "marka": "PLATAPRO",
+        "model": "Model2",
+        "renk": "Gumus",
+        "tasarim": "Kalp",
+        "fiyat": 1200,
+        "detay": {
+            "malzeme": "Gümüş",
+            "tasarim_detayi": "Kalp şeklinde tasarım",
+            "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+        },
+        "img_url": "IMG/YUZUK2.jpeg"
+    }, 
+    {
+            "urun_id": "yuzuk_002",
+            "marka": "PLATAPRO",
+            "model": "Model2",
+            "renk": "Gumus",
+            "tasarim": "Kalp",
+            "fiyat": 1200,
+            "detay": {
+                "malzeme": "Gümüş",
+                "tasarim_detayi": "Kalp şeklinde tasarım",
+                "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+            },
+            "img_url": "IMG/yuzuktugrali.jpg" }, 
+             {
+                "urun_id": "yuzuk_002",
+                "marka": "PLATAPRO",
+                "model": "Model2",
+                "renk": "Gumus",
+                "tasarim": "Kalp",
+                "fiyat": 1200,
+                "detay": {
+                    "malzeme": "Gümüş",
+                    "tasarim_detayi": "Kalp şeklinde tasarım",
+                    "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+                },
+                "img_url": "IMG/maviyuzuk.webp" },  
+                {
+                    "urun_id": "yuzuk_002",
+                    "marka": "YAGIZ BARAN",
+                    "model": "Model2",
+                    "renk": "Gumus",
+                    "tasarim": "Kalp",
+                    "fiyat": 1200,
+                    "detay": {
+                        "malzeme": "Gümüş",
+                        "tasarim_detayi": "Kalp şeklinde tasarım",
+                        "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+                    },
+                    "img_url": "IMG/yuzuka.jpeg" },  
+                    {
+                        "urun_id": "yuzuk_002",
+                        "marka": "PLATAPRO",
+                        "model": "Model2",
+                        "renk": "Gumus",
+                        "tasarim": "Kalp",
+                        "fiyat": 1200,
+                        "detay": {
+                            "malzeme": "Gümüş",
+                            "tasarim_detayi": "Kalp şeklinde tasarım",
+                            "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+                        },
+                        "img_url": "IMG/YUZUK7.jpeg" },  {
+                            "urun_id": "yuzuk_002",
+                            "marka": "PLATAPRO",
+                            "model": "Model2",
+                            "renk": "Gumus",
+                            "tasarim": "Kalp",
+                            "fiyat": 1200,
+                            "detay": {
+                                "malzeme": "Gümüş",
+                                "tasarim_detayi": "Kalp şeklinde tasarım",
+                                "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+                            },
+                            "img_url": "IMG/yuzukaltinceyrek.jpg" }, 
+                             {
+            "urun_id": "yuzuk_002",
+            "marka": "PLATAPRO",
+            "model": "Model2",
+            "renk": "Gumus",
+            "tasarim": "Kalp",
+            "fiyat": 1200,
+            "detay": {
+                "malzeme": "Gümüş",
+                "tasarim_detayi": "Kalp şeklinde tasarım",
+                "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+            },
+            "img_url": "IMG/yuzukc.jpeg" },  {
+                "urun_id": "yuzuk_002",
+                "marka": "PLATAPRO",
+                "model": "Model2",
+                "renk": "Gumus",
+                "tasarim": "Kalp",
+                "fiyat": 1200,
+                "detay": {
+                    "malzeme": "Gümüş",
+                    "tasarim_detayi": "Kalp şeklinde tasarım",
+                    "tasima_ozelligi": "Özel kolye kutusunda güvenli bir şekilde taşınabilir"
+                },
+                "img_url": "IMG/yuzukb.jpeg" }, 
+               
+            ];
